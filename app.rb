fb9e7fbf3ba68cc5a5e8ba287374ed19814b946a
github username <github email address>
2018-09-18 12:39:49 -0400

    Done.

--- app.rb
+++ app.rb
@@ -30,6 +30,7 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
+    binding.pry
     operation = params[:operation]
     number1 = params[:number1].to_i
     number2 = params[:number2].to_i
